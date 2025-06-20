6f80aba66af535eb5b7504fd0feeab852028d00c
Breland Massey <breland@Brelands-MacBook-Pro.local>
2025-06-20 23:15:52 +0300

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const App = () => <div style={{ color: 'white', backgroundColor: 'black', padding: 20 }}>Hello Maxxima!</div>;
+
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
